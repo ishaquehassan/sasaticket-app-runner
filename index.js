07cc786d4ac4e9f15f9bbc1ed8167cd75b987d37
hackerhgl <hackerhgl@gmail.com>
2021-07-06 01:04:40 +0500

    Timestamp writing in files

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const moment = require('moment');
+const fs = require('fs');
 const args = require('args-parser')(process.argv);
+
 
 
 async function main()  {
     try {
-        console.log(args);
-        //
+        const timestamp = moment().format('Y-MMM-DD HH:mm:A');
+        const file = fs.writeFileSync('date.txt', timestamp)
     } catch (error) {
         console.error(error);
         process.exit(1);
