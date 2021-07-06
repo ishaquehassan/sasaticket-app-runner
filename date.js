1818cf42b34b4de0a32f84a9d72d5eaf9a6b1d26
hackerhgl <hackerhgl@gmail.com>
2021-07-06 16:05:58 +0500

    test

--- date.js
+++ date.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 async function main()  {
     try {
-        const timestamp = moment().format('Y-MMM-DD HH:mm:A');
+        const timestamp = moment().format('Y-MMM-DD HH:mm:ss:A');
         fs.writeFileSync('date.txt', timestamp);
         process.exit();
     } catch (error) {
