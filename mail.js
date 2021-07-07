3a81a5ef607d6c952da4188d856b5931db9466e3
hackerhgl <hackerhgl@gmail.com>
2021-07-07 19:47:15 +0500

    trigger

--- mail.js
+++ mail.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 async function main(){
     try {
         const configs = JSON.parse(args.configs);
-        const timestamp = "2021-Jul-07 11:58:AM";
+        // const timestamp = "2021-Jul-07 11:58:AM";
         const timestamp = fs.readFileSync('date.txt', 'utf8');
         const dir = await drive.getFolderByName(timestamp);
         // console.log(dir.data.files[0]);
